d8a73cb9ea70fdebfdc508038ef92a5ad02ee20b
jasazupancic123 <jasa.zupancic@student.um.si>
2025-02-10 17:58:36 +0100

    fix: fix some buggs after merge

--- fitcode-frontend-next/src/components/hero-navbar.tsx
+++ fitcode-frontend-next/src/components/hero-navbar.tsx
@@ -50,7 +50,7 @@ export default function HeroNavbar({ showLogin = true }) {
             }}
           >
             <Box sx={{ flexGrow: 1, display: 'flex', alignItems: 'center' }}>
-              <Logo width={120} height={40} />
+              <Logo width={120} height={35} />
               <Box mr={4} />
 
               <Box
