0864405036064ae387822e5a9193b283c46ef810
jeanbabtista <68808279+jeanbabtista@users.noreply.github.com>
2025-02-26 17:55:54 +0100

    REMOVE CONSOLE LOG FROM JASA

--- fitcode-frontend-next/src/app/groups/[group_id]/trainer-group-cycle-view.tsx
+++ fitcode-frontend-next/src/app/groups/[group_id]/trainer-group-cycle-view.tsx
@@ -35,8 +35,6 @@ export default function TrainerCycleView() {
   const [selectedComponents, setSelectedComponents] = useState<Component[]>([]);
   const [isSticky, setIsSticky] = useState(false);
 
-  console.log('cycle', cycle);
-
   // effect to track scroll position and set sticky mode
   useEffect(() => {
     const handleScroll = () => {
