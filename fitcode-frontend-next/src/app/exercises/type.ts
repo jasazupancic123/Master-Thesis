5e61f4e4dbacbe90a64c3e6ee7234ced0453ae2a
jasazupancic123 <jasa.zupancic@student.um.si>
2025-04-24 13:02:41 +0200

    fix: add some fixes

--- fitcode-frontend-next/src/app/exercises/type.ts
+++ fitcode-frontend-next/src/app/exercises/type.ts
@@ -1,16 +1,16 @@
 import { SetState } from '@/common/type/state.type';
+import { Attribute } from '@/controller/attribute/type/attribute.type';
 import { Component } from '@/controller/component/type/component.type';
-import { ExerciseAttribute } from '@/controller/exercise/type/exercise-attribute.type';
 import { Exercise } from '@/controller/exercise/type/exercise.type';
 
 export interface ExercisePageProps {
   token: string;
   exercises: Exercise[];
-  attributes: ExerciseAttribute[];
+  attributes: Attribute[];
   components: Component[];
 }
 
 export interface ExerciseContextProps extends ExercisePageProps {
   setExercises: SetState<Exercise[]>;
-  setAttributes: SetState<ExerciseAttribute[]>;
+  setAttributes: SetState<Attribute[]>;
 }
