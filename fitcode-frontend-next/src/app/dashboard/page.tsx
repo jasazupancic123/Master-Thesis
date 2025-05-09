129ea917bf764042d05ca80d341af971ffe3afb5
Jean <bedrac.zan@gmail.com>
2025-05-09 20:43:50 +0200

    chore: merge workout and cooldown feature

--- fitcode-frontend-next/src/app/dashboard/page.tsx
+++ fitcode-frontend-next/src/app/dashboard/page.tsx
@@ -1,52 +1,39 @@
-'use server';
-
 import { FIREBASE_COOKIE_NAME } from '@/common/constant/browser.constant';
-import { GroupController } from '@/controller/group/group.controller';
 import { UserController } from '@/controller/user/user.controller';
 import { cookies } from 'next/headers';
 import Dashboard from './dashboard';
-import { Organization } from '@/controller/organization/type/organization.type';
-import { DashboardProvider } from '@/context/dashboard-provider';
+import {
+  DashboardPageProps,
+  DashboardProvider,
+} from '@/context/dashboard-provider';
+import { InstitutionController } from '@/controller/institution/institution.controller';
+import { UserRole } from '@/controller/user/enum/user-role.enum';
 
 export default async function Page() {
   const cookieStore = await cookies();
   const token = cookieStore.get(FIREBASE_COOKIE_NAME)?.value;
   if (!token) return <div>Unauthorized</div>;
 
-  const profile = await UserController.findMe(token);
-  if (!profile) return <div>Unauthorized</div>;
+  const user = await UserController.findMe(token);
+  if (!user) return <div>Unauthorized</div>;
 
-  const role = profile.customClaims.role[0];
-  const [users, groups] = await Promise.all([
-    UserController.findAll(token),
-    GroupController.findAll(token),
+  const [institutions, groups] = await Promise.all([
+    InstitutionController.findAllByUser(token),
+    InstitutionController.findAllGroups(token),
   ]);
 
-  const organizations: Organization[] = [
-    {
-      id: '1',
-      name: 'NK Maribor',
-      manager: { ...profile },
-      // trainers: Array.from({ length: 12 }, () => ({ ...profile })),
-      trainers: [{ ...profile }],
-      groups: groups,
-      // groups: Array.from({ length: 24 }, () => ({ ...groups[0] })),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    },
-  ];
+  const props: DashboardPageProps = {
+    token,
+    user,
+    institutions,
+    users: institutions.flatMap((i) => i.members),
+  };
 
-  const organization: Organization = { ...organizations[0] };
+  const role = user.customClaims.role[0];
+
   return (
-    <DashboardProvider>
-      <Dashboard
-        organization={organization}
-        organizations={organizations}
-        role={role}
-        users={users}
-        token={token}
-        profile={profile}
-      />
+    <DashboardProvider {...props}>
+      {role === UserRole.MANAGER ? <Dashboard /> : <>Trainer View</>}
     </DashboardProvider>
   );
 }
