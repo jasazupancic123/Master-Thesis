c16240c89d40b0a51a9f1ea3c5a303cd46282b34
jasazupancic123 <jasa.zupancic@student.um.si>
2025-05-19 11:16:10 +0200

    fix: small bugfix

--- fitcode-frontend-next/src/components/trainer-day-view/training-component-calendar.tsx
+++ fitcode-frontend-next/src/components/trainer-day-view/training-component-calendar.tsx
@@ -1,23 +1,13 @@
 import { SetState } from '@/common/type/state.type';
-import ExerciseChips from '@/components/exercise-chips';
-import SelectInput from '@/components/select-input';
-import {
-  handleAddTrainingComponents,
-  handleDeleteTrainingComponent,
-} from '@/components/trainer-cycle-view/state';
 import TrainingWeek from '@/components/training-cycle-view-week/training-week';
 import { useGroup } from '@/context/group-provider';
-import { ComponentService } from '@/controller/component/component.service';
 import { Component } from '@/controller/component/type/component.type';
-import { Cycle } from '@/controller/group/type/cycle.type';
 import { TrainingComponent } from '@/controller/training/type/training-plan.type';
 import { Training } from '@/controller/training/type/training.type';
-import { RotateRight } from '@mui/icons-material';
 import { Typography, useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import dayjs from 'dayjs';
-import { useRouter } from 'next/navigation';
 import React, { Fragment, useEffect, useState } from 'react';
 import InfoIcon from '@mui/icons-material/Info';
 
@@ -44,58 +34,46 @@ export default function TrainingComponentCalendar(
     handleCopyComponentApiRequest,
   } = props;
 
-  const {
-    token,
-    group,
-    components,
-    cycle,
-    setCycle,
-    setTrainings,
-    setFilteredTrainings,
-    setDateFrom,
-    setDateTo,
-  } = useGroup();
+  const { cycle } = useGroup();
 
   const theme = useTheme();
-  const router = useRouter();
   const [selectedComponents, setSelectedComponents] = useState<Component[]>([]);
-  const [isSticky, setIsSticky] = useState(false);
-
-  // effect to track scroll position and set sticky mode
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      setIsSticky(scrollY > 150);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
-  // filter trainings by cycle
-  useEffect(() => {
-    if (!cycle) return;
-    setDateFrom(dayjs(cycle.from));
-    setDateTo(dayjs(cycle.to));
-  }, [cycle]);
 
   return (
     <Box>
-      {trainingComponent.component ? (
-        <Box display="flex" width="100%" alignItems="center" flexDirection="column">
-          <Typography variant="h6">{trainingComponent.component?.name[0].toUpperCase() +
-            trainingComponent.component?.name.slice(1)}{' '}
-          Calendar
+      {trainingComponent.component && (
+        <Box
+          display="flex"
+          width="100%"
+          alignItems="center"
+          flexDirection="column"
+        >
+          <Typography variant="h6">
+            {trainingComponent.component?.name[0].toUpperCase() +
+              trainingComponent.component?.name.slice(1)}{' '}
+            Calendar
           </Typography>
-          <Typography variant="body2" color="text.secondary" mt={1} display="flex" alignItems="center" gap={0.5}>
-            <InfoIcon/> Colored cells already have a training in period
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            mt={1}
+            display="flex"
+            alignItems="center"
+            gap={0.5}
+          >
+            <InfoIcon /> Colored cells already have a training in period
           </Typography>
-          <Typography variant="body2" color="text.secondary" mt={1} display="flex" alignItems="center" gap={0.5}>
-             Copying the outlined component
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            mt={1}
+            display="flex"
+            alignItems="center"
+            gap={0.5}
+          >
+            Copying the outlined component
           </Typography>
         </Box>
-      ) : (
-        <></>
       )}
 
       {/* Choose cycle */}
