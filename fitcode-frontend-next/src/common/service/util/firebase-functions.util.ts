601cd94264796ac414be5113a260eef1e9015698
Jean <bedrac.zan@gmail.com>
2025-06-18 13:33:38 +0300

    feat: add new firebase function to register new users and remove old auth trigger

---
+++ fitcode-frontend-next/src/common/service/util/firebase-functions.util.ts
@@ -0,0 +1,39 @@
+import { httpsCallable } from 'firebase/functions';
+import { functions } from '@/common/config/firebase.config';
+import { UserRole } from '@/controller/user/enum/user-role.enum';
+
+export class FirebaseFunctionsUtil {
+  async createUserWithRole(input: {
+    email: string;
+    password: string;
+    role: UserRole;
+  }) {
+    try {
+      await httpsCallable(functions, 'createUserWithRole')(input);
+    } catch (e: any) {
+      console.log(JSON.stringify(e));
+      if (e.code) {
+        const authCode = (e.details as { originalCode: string })?.originalCode;
+        switch (authCode) {
+          case 'auth/invalid-credential':
+          case 'auth/user-not-found':
+          case 'auth/wrong-password':
+          case 'auth/invalid-email':
+          case 'auth/invalid-password':
+          case 'auth/weak-password':
+            throw new Error('Email or password is incorrect');
+          case 'auth/email-already-exists':
+            throw new Error('Email already exists');
+          case 'auth/app-deleted':
+          case 'auth/app-not-authorized':
+          case 'auth/argument-error':
+          case 'auth/invalid-api-key':
+          case 'auth/operation-not-allowed':
+            throw new Error('Internal error');
+        }
+
+        throw new Error(e.message);
+      }
+    }
+  }
+}
