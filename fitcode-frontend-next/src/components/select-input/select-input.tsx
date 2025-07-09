0fdb20b20ce2b932dbabf6326a2f1ce4ff931925
jasazupancic123 <jasa.zupancic@student.um.si>
2025-07-09 11:54:45 +0200

    feat: new year design

--- fitcode-frontend-next/src/components/select-input/select-input.tsx
+++ fitcode-frontend-next/src/components/select-input/select-input.tsx
@@ -31,6 +31,8 @@ interface Props<T> {
   selectedItemSize?: number;
   iconSize?: number;
   sameValueAction?: boolean;
+  dissableMinWidth?: boolean;
+  maxWidth?: string | number;
 }
 
 export default function SelectInput<T>(props: Props<T>) {
@@ -41,12 +43,16 @@ export default function SelectInput<T>(props: Props<T>) {
         props.sx
           ? {
               ...props.sx,
-              minWidth: 120,
+              minWidth: props.dissableMinWidth ? undefined : 120,
+              maxWidth: props.maxWidth,
               '& .MuiSelect-icon': {
                 fontSize: props.iconSize,
               },
             }
-          : { minWidth: 120 }
+          : {
+              minWidth: props.dissableMinWidth ? undefined : 120,
+              maxWidth: props.maxWidth,
+            }
       }
     >
       {props.disableInputLabel === true ? (
