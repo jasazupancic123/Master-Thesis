e9b84abe3c30c609a1e5a8b5463b90d23aa1ed37
jasazupancic123 <jasa.zupancic@student.um.si>
2025-07-15 14:02:12 +0200

    feat: implement session wellness cache

---
+++ fitcode-frontend-next/src/session-cache/wellness.session-cache.ts
@@ -0,0 +1,11 @@
+import { Wellness } from '@/controller/user/type/wellness.type';
+
+let cachedWellness: Wellness | null = null;
+
+export function getCachedWellness() {
+  return cachedWellness;
+}
+
+export function setCachedWellness(wellness: Wellness) {
+  cachedWellness = wellness;
+}
