5b90a163d3aac0274f94c67fe1a134b3b04608f6
jasazupancic123 <jasa.zupancic@student.um.si>
2025-07-17 10:51:18 +0200

    style: add dashboard institution view

--- fitcode-frontend-next/src/sites/dashboard.institution.tsx
+++ fitcode-frontend-next/src/sites/dashboard.institution.tsx
@@ -4,19 +4,27 @@ import { theme } from '@/app/style';
 import HorizontalItemsList from '@/components/horizontal-items-list/horizontal-items-list';
 import { MAX_WIDTH } from '@/components/trainer-day-view/constant';
 import { useDashboard } from '@/store/dashboard-provider';
-import { MoreVert } from '@mui/icons-material';
-import { Box, IconButton, Typography } from '@mui/material';
+import { Add, MoreVert, Remove } from '@mui/icons-material';
+import { Avatar, Box, IconButton, Typography } from '@mui/material';
 import { useScreenSize } from '@/store/screen-size-provider';
 import { AthletesTrainers } from '@/common/enum/athletes-trainer.enum';
 import { useEffect, useState } from 'react';
 import { SearchBar } from '@/components/search-bar/search-bar';
 import { User } from '@/controller/user/type/user.type';
+import MyModal from '@/components/modal/modal';
+import RegisterUsersDashboard from '@/components/dashboard-register-users-modal/dashboard-register-users-modal';
 import { UserRole } from '@/controller/user/enum/user-role.enum';
+import { isManager } from '@/common/service/util/firebase-auth.util';
+import { handleApiRequest } from '@/common/type/state.type';
+import { InstitutionController } from '@/controller/institution/institution.controller';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
 
 export default function DashboardInstitutionPage() {
-  const { users, selectedInstitution, selectedGroup, setSelectedGroup } =
+  const { token, profile, selectedInstitution, setSelectedInstitution } =
     useDashboard();
   const screenSize = useScreenSize();
+  const router = useRouter();
 
   const [selectedView, setSelectedView] = useState<AthletesTrainers>(
     AthletesTrainers.ATHLETES
@@ -25,23 +33,70 @@ export default function DashboardInstitutionPage() {
   const [currentUsers, setCurrentUsers] = useState<User[]>([]);
   const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
   const [search, setSearch] = useState('');
+  const [openModal, setOpenModal] = useState(false);
+  const [hoveredUser, setHoveredUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const roles = profile.customClaims.role || [];
 
   useEffect(() => {
-    if (!users) return;
+    const current =
+      selectedView === AthletesTrainers.ATHLETES
+        ? selectedInstitution?.athletes
+        : (selectedInstitution?.trainers ?? []);
 
-    const current = users.filter((user) => {
-      const role =
-        selectedView === AthletesTrainers.ATHLETES
-          ? UserRole.ATHLETE
-          : UserRole.TRAINER;
-      if (user.customClaims.role.includes(role)) {
-        return user;
-      }
-    });
+    if (!current) return;
 
     setSearch('');
     setCurrentUsers(current);
-  }, [selectedView]);
+    setFilteredUsers(current);
+    setLoading(false);
+  }, [selectedView, selectedInstitution]);
+
+  const handleRemoveUser = (userId: string, view: AthletesTrainers) => {
+    if (!selectedInstitution) return;
+
+    handleApiRequest(
+      router,
+      () =>
+        view === AthletesTrainers.ATHLETES
+          ? InstitutionController.removeAthletes(
+              token,
+              selectedInstitution.id,
+              {
+                athleteIds: [userId],
+              }
+            )
+          : InstitutionController.removeTrainers(
+              token,
+              selectedInstitution.id,
+              {
+                trainerIds: [userId],
+              }
+            ),
+      (institution) => {
+        setSelectedInstitution((prev) => {
+          if (!prev) return null;
+          const updatedUsers = (
+            view === AthletesTrainers.ATHLETES ? prev.athletes : prev.trainers
+          )?.filter((a) => a.uid !== userId);
+          const updatedUserIds =
+            view === AthletesTrainers.ATHLETES
+              ? prev.athleteIds.filter((id) => id !== userId)
+              : prev.trainerIds.filter((id) => id !== userId);
+
+          return view === AthletesTrainers.ATHLETES
+            ? { ...prev, athletes: updatedUsers, athleteIds: updatedUserIds }
+            : { ...prev, trainers: updatedUsers, trainerIds: updatedUserIds };
+        });
+        toast.success(
+          `${view[0].toUpperCase() + view.slice(1, view.length - 1).toLowerCase()} removed successfully`
+        );
+      },
+      undefined,
+      `Failed to remove ${view[0].toUpperCase() + view.slice(1, view.length - 1).toLowerCase()}`
+    );
+  };
 
   return (
     <Box
@@ -50,9 +105,10 @@ export default function DashboardInstitutionPage() {
       justifyContent="center"
       width="100%"
       maxWidth={MAX_WIDTH}
-      gap={2}
+      gap={screenSize.isSmallTablet || screenSize.isMobile ? 0 : 5}
       sx={{
         backgroundColor: theme.palette.background.default,
+        pb: 2,
       }}
     >
       {screenSize.isSmallTablet || screenSize.isMobile ? (
@@ -81,9 +137,18 @@ export default function DashboardInstitutionPage() {
               display="flex"
               justifyContent="center"
               alignItems="center"
-              mt={1}
-              sx={{ mx: 'auto', mb: 1 }}
+              mt={2}
+              gap={0.75}
+              sx={{ mx: 'auto' }}
             >
+              <Box
+                sx={{
+                  height: 16,
+                  width: 4,
+                  borderRadius: 5,
+                  backgroundColor: theme.palette.primary.main,
+                }}
+              />
               <Typography
                 fontWeight={600}
                 fontSize={16}
@@ -130,6 +195,7 @@ export default function DashboardInstitutionPage() {
                 width: 4,
                 borderRadius: 5,
                 backgroundColor: theme.palette.primary.main,
+                ml: 1,
               }}
             />
 
@@ -173,16 +239,16 @@ export default function DashboardInstitutionPage() {
       <Box
         width="100&"
         display="flex"
+        gap={1}
         sx={{
           justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
         <SearchBar
           placeholder={`Search ${selectedView.toLowerCase()}`}
           value={search}
           handleSearchChange={(e) => {
-            if (!users) return;
-
             const filtered = currentUsers.filter((user) => {
               if (!user.displayName) return false;
               return user.displayName
@@ -193,9 +259,138 @@ export default function DashboardInstitutionPage() {
             setFilteredUsers(filtered);
             setSearch(e.target.value);
           }}
-          maxWidth="85%"
+          maxWidth={
+            screenSize.isSmallTablet || screenSize.isMobile ? '50%' : '85%'
+          }
+          sx={{
+            my: screenSize.isSmallTablet || screenSize.isMobile ? 2 : 0,
+          }}
         />
+        {isManager(roles) && (
+          <IconButton
+            sx={{
+              m: 0,
+              p: 0.5,
+              backgroundColor: theme.palette.background.light,
+              borderRadius: 1,
+            }}
+            onClick={() => setOpenModal(true)}
+          >
+            <Add fontSize="small" />
+          </IconButton>
+        )}
       </Box>
+      <Box width="100%" display="flex" flexDirection="column">
+        <Box
+          width="100%"
+          sx={{
+            height: 7,
+            backgroundColor: theme.palette.background.paper,
+          }}
+        />
+        <Box
+          width="100%"
+          display="flex"
+          flexWrap="wrap"
+          gap={screenSize.isMobile ? 4 : 6}
+          sx={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'relative',
+            mt: 2,
+            px: 2,
+          }}
+        >
+          {loading ? (
+            <></>
+          ) : !currentUsers.length ? (
+            <Typography>No {selectedView.toLowerCase()} found</Typography>
+          ) : (
+            filteredUsers.map((user) => {
+              if (!user || !user.displayName) return;
+
+              const names = user.displayName.split(' ');
+
+              return (
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  gap={1}
+                  sx={{
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    position: 'relative',
+                  }}
+                  onMouseEnter={() => setHoveredUser(user)}
+                  onMouseLeave={() => setHoveredUser(null)}
+                >
+                  {isManager(roles) && user.uid === hoveredUser?.uid && (
+                    <IconButton
+                      className="remove-icon"
+                      size="small"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        handleRemoveUser(user.uid, selectedView);
+                      }}
+                      sx={{
+                        position: 'absolute',
+                        top: -8,
+                        right: -8,
+                        backgroundColor: theme.palette.error.main,
+                        zIndex: 1,
+                      }}
+                    >
+                      <Remove sx={{ fontSize: 10 }} />
+                    </IconButton>
+                  )}
+                  <Avatar
+                    className="avatar-border"
+                    //src={user.profileImageUrl || '/user_avatar.png'}
+                    src="/user_avatar.png"
+                    sx={{
+                      width: screenSize.isMobile ? 70 : 80,
+                      height: screenSize.isMobile ? 70 : 80,
+                    }}
+                  />
+                  <Typography
+                    variant="body2"
+                    sx={{
+                      textAlign: 'center',
+                      fontWeight: 400,
+                      fontSize: screenSize.isMobile ? 12 : 14,
+                    }}
+                  >
+                    {names.length > 1 ? (
+                      <>
+                        {names[0]}
+                        <br />
+                        {names[1].toUpperCase()}
+                      </>
+                    ) : (
+                      <>{user.displayName.toUpperCase()}</>
+                    )}
+                  </Typography>
+                </Box>
+              );
+            })
+          )}
+        </Box>
+      </Box>
+      <MyModal
+        isOpen={openModal}
+        setIsOpen={(open) => setOpenModal(open)}
+        onConfirm={undefined}
+        onCancel={() => setOpenModal(false)}
+        cancelText="Close"
+      >
+        <RegisterUsersDashboard
+          registerRole={
+            selectedView === AthletesTrainers.ATHLETES
+              ? UserRole.ATHLETE
+              : UserRole.TRAINER
+          }
+        />
+      </MyModal>
     </Box>
   );
 }
