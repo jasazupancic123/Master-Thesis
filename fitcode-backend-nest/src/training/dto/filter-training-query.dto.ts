57a663187d3f0b1e6901e3d02ded350cfe42d6c1
jeanbabtista <bedrac.zan@gmail.com>
2025-07-30 08:49:56 +0200

    chore: remove 'minimal' filter when fetching trainings

--- fitcode-backend-nest/src/training/dto/filter-training-query.dto.ts
+++ fitcode-backend-nest/src/training/dto/filter-training-query.dto.ts
@@ -1,7 +1,4 @@
 import { IntersectionType, PartialType, PickType } from '@nestjs/mapped-types';
-import { ApiPropertyOptional } from '@nestjs/swagger';
-import { Expose } from 'class-transformer';
-import { IsBoolean, IsOptional } from 'class-validator';
 
 import { DateFilterDto } from '@src/common/dto/date-filter.dto';
 
@@ -10,10 +7,4 @@ import { Training } from '../entity/training.entity';
 export class FilterTrainingQueryDto extends IntersectionType(
   PartialType(PickType(Training, ['groupId', 'cycleId'] as const)),
   DateFilterDto,
-) {
-  @IsBoolean()
-  @IsOptional()
-  @ApiPropertyOptional()
-  @Expose()
-  minimal?: boolean;
-}
+) {}
