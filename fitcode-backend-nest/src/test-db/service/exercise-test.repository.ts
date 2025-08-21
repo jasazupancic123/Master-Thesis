f818daed821f4e0a677b2fd43dfa4091ea1c4b82
jeanbabtista <bedrac.zan@gmail.com>
2025-08-21 10:37:14 +0200

    fix: exercise test mixin

--- fitcode-backend-nest/src/test-db/service/exercise-test.repository.ts
+++ fitcode-backend-nest/src/test-db/service/exercise-test.repository.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
+import { Exercise } from '@src/exercise/entity/exercise.entity';
 import { ExerciseRepository } from '@src/exercise/repository/exercise.repository';
 
 import { TestRepositoryMixin } from '../test-repository.mixin';
 
 @Injectable()
-export class ExerciseTestRepository extends TestRepositoryMixin(
+export class ExerciseTestRepository extends TestRepositoryMixin<Exercise>()(
   ExerciseRepository,
 ) {}
