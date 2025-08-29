f0f5a1009713dc2eb1c655281164e7ab6e8d97f5
jeanbabtista <bedrac.zan@gmail.com>
2025-08-29 23:58:06 +0200

    feat: add aphosting firebase service

--- fitcode-backend-nest/test/teardown.ts
+++ fitcode-backend-nest/test/teardown.ts
@@ -14,7 +14,7 @@ export default async function () {
   const commonService = new CommonService();
   const configService = new ConfigService<Environment>();
 
-  const credential = JSON.parse(configService.get('FIREBASE_CREDENTIALS'));
+  const credential = JSON.parse(configService.get('FIREBASE_CONFIG'));
   const firebaseAdminClient = getFirebaseClient({ credential });
   const firebase = new FirebaseService(
     configService,
