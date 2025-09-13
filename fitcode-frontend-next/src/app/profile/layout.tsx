84cea5bd047ad7c2f8769cc06740144ed596f814
jeanbabtista <bedrac.zan@gmail.com>
2025-09-13 12:12:35 +0300

    deploy: disable connection await in root layout

--- fitcode-frontend-next/src/app/profile/layout.tsx
+++ fitcode-frontend-next/src/app/profile/layout.tsx
@@ -6,7 +6,6 @@ import Container from '@mui/material/Container';
 import type { ChildrenProps } from '@/common/type/props.type';
 import ProfileInitializer from '@/initializers/profile.initializer';
 import { withAuth } from '@/store/auth.provider';
-import MainProvider from '@/store/main.provider';
 
 export default withAuth(Layout);
 
@@ -14,9 +13,7 @@ function Layout({ children }: ChildrenProps) {
   return (
     <Box bgcolor="background.default">
       <Container component="main" maxWidth="lg">
-        <MainProvider>
-          <ProfileInitializer>{children}</ProfileInitializer>
-        </MainProvider>
+        <ProfileInitializer>{children}</ProfileInitializer>
       </Container>
     </Box>
   );
