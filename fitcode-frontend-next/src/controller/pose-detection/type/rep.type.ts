3bd58b9d55fe8e52de85deeed09b5e8c3e178ee8
jasazupancic123 <jasa.zupancic@student.um.si>
2025-09-18 12:42:14 +0300

    feat: implement post rep window to track extra extremums

--- fitcode-frontend-next/src/controller/pose-detection/type/rep.type.ts
+++ fitcode-frontend-next/src/controller/pose-detection/type/rep.type.ts
@@ -1,17 +1,25 @@
 import type { KeypointHistory } from '../class/keypoint-history';
 
 export type Rep = {
-  // set on init
+  // INIT
   repNumber: number; // id
   createdAt: Date; // when the rep was created, used for idle time
   startValue: number; // the value when the rep started
+  startValueFrameNum: number; // the frame number when the rep started
   buffer: KeypointHistory; // the buffer of keypoints during the rep
   detectedExtremum: boolean; // if the rep detected an extremum (local max or min)
 
-  // set later
+  // OPTIONAL
+  // endValue
   endValue?: number; // the value when the rep ended
+  endValueFrameNum?: number; // the frame number when the rep ended
+
+  // extremeValue
   extremeValue?: number; // the max or min value reached during the rep, also needs to be out of a certain range from the start value
+  extremeValueFrameNum?: number; // the frame number when the extreme value was reached
   extremeValueIndex?: number; // the index in the repBuffer of the extremeValue
+
+  // times
   startTime?: Date; // when the value starts decending/ascending
   endTime?: Date; // when the value comes back to the starting point
   idleTime?: number; // startTime - createdAt
