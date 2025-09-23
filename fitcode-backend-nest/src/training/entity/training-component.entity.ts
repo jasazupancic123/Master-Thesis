c5ffb60cbc76cc34254c40c5bce88524080a25a0
jeanbabtista <bedrac.zan@gmail.com>
2025-09-23 12:37:54 +0300

    docs: add swagger docs for valira endpoints

--- fitcode-backend-nest/src/training/entity/training-component.entity.ts
+++ fitcode-backend-nest/src/training/entity/training-component.entity.ts
@@ -37,7 +37,11 @@ export class TrainingComponent extends IntersectionType(IdEntity, ColorEntity) {
 
   @IsEnum(MainSet)
   @IsString()
-  @ApiProperty({ type: () => MainSet })
+  @ApiProperty({
+    enum: MainSet,
+    enumName: 'MainSet',
+    description: 'Type of main set',
+  })
   @Expose()
   mainSet: MainSet; // defaults to "block"
 
