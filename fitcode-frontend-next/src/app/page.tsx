713d98f7ba8d664b79dc365313f81d221b20ee6b
jasazupancic123 <jasa.zupancic@student.um.si>
2025-09-23 17:30:15 +0300

    style: new landing and sign in page

--- fitcode-frontend-next/src/app/page.tsx
+++ fitcode-frontend-next/src/app/page.tsx
@@ -1,19 +1,16 @@
 'use client';
 
 import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import { redirect, RedirectType } from 'next/navigation';
 import React from 'react';
 
 import { theme } from '@/app/style';
-import {
-  LINKS_NAVBAR,
-  SIGN_IN_REDIRECT_MAPPER,
-} from '@/common/constant/navigation.constant';
+import { SIGN_IN_REDIRECT_MAPPER } from '@/common/constant/navigation.constant';
 import type { ChildrenProps } from '@/common/type/props.type';
 import HeroNavbar from '@/components/hero-navbar/hero-navbar';
 import { useAuth } from '@/store/auth.provider';
+import { HERO_NAVBAR_HEIGHT } from './state';
 
 export type AppPageProps = ChildrenProps & {
   title: string;
@@ -29,87 +26,39 @@ export default function Home() {
   else
     return (
       <>
-        <HeroNavbar />
+        <HeroNavbar height={HERO_NAVBAR_HEIGHT} />
 
         {/* Hero */}
         <Box
-          id={LINKS_NAVBAR.index.id}
           sx={{
             width: '100%',
-            height: '100vh',
+            height: `calc(100vh - ${HERO_NAVBAR_HEIGHT})`,
             position: 'relative',
-            backgroundSize: '100% 5%',
-            backgroundRepeat: 'no-repeat',
+            backgroundColor: theme.palette.primary.main,
+            mt: HERO_NAVBAR_HEIGHT,
+            overflowX: 'hidden',
+            overflowY: 'hidden',
           }}
         >
-          <Box
+          <Typography
+            component="div"
+            fontSize={`calc(100vh - ${HERO_NAVBAR_HEIGHT})`}
             sx={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              zIndex: -1,
-              backgroundColor: 'black',
+              fontWeight: 800,
+              display: 'inline-block',
+              lineHeight: 1, // or 1.1
+              color: theme.palette.text.secondary,
+              textAlign: 'center',
+              whiteSpace: 'nowrap',
+              animation: 'scroll-left 6s linear infinite',
+              '@keyframes scroll-left': {
+                '0%': { transform: 'translateX(42.5%)' },
+                '100%': { transform: 'translateX(-100%)' },
+              },
             }}
           >
-            <video
-              autoPlay
-              loop
-              muted
-              playsInline
-              style={{
-                position: 'absolute',
-                top: '50%',
-                left: '50%',
-                width: '100%',
-                height: '100%',
-                objectFit: 'cover',
-                transform: 'translate(-50%, -50%)',
-                zIndex: -1, // Ensure the video stays in the background
-                filter: 'grayscale(100%) brightness(80%)',
-              }}
-            >
-              <source src="/hero.mp4" type="video/mp4" />
-            </video>
-          </Box>
-
-          <Container
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-              height: '100%',
-            }}
-          >
-            <Box sx={{ textAlign: 'center' }}>
-              <Typography
-                component="span"
-                variant="h3"
-                fontWeight="bold"
-                sx={{
-                  color: theme.palette.primary.main,
-                  fontSize: '3rem',
-                  textTransform: 'uppercase',
-                }}
-              >
-                Do it right
-              </Typography>
-
-              <br />
-
-              <Typography
-                component="span"
-                variant="h4"
-                sx={{
-                  color: '#FFF',
-                  fontSize: '2.0rem',
-                }}
-              >
-                Coming Soon
-              </Typography>
-            </Box>
-          </Container>
+            DO IT RIGHT
+          </Typography>
         </Box>
       </>
     );
