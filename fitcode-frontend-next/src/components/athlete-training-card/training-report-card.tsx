2912e83a2750570bd19e38c4f73bf6459d6a3b1f
jasazupancic123 <jasa.zupancic@student.um.si>
2025-09-24 12:08:34 +0300

    feat: add training reports graphs

--- fitcode-frontend-next/src/components/athlete-training-card/training-report-card.tsx
+++ fitcode-frontend-next/src/components/athlete-training-card/training-report-card.tsx
@@ -4,7 +4,12 @@ import React, { useState } from 'react';
 
 import type { TrainingReport } from '@/controller/training/type/training-report.type';
 import AthleteTrainingCardHeader from './athlete-training-card-header';
-import { PieChart } from '@mui/x-charts';
+import {
+  BarPlot,
+  ChartContainer,
+  ChartsTooltip,
+  PieChart,
+} from '@mui/x-charts';
 import { PieCenterLabel } from '@/common/util/mui-charts.util';
 import { ChildrenProps } from '@/common/type/props.type';
 import { theme } from '@/app/style';
@@ -19,6 +24,12 @@ export default function TrainingReportCard(props: TrainingReportCardProps) {
 
   const [realizationScore] = useState(
     Math.round((report.realizationScore / report.totalRealizationScore) * 100)
+  );
+  const [tonnageScore] = useState(
+    Math.round((report.tonnage / report.totalTonnage) * 100)
+  );
+  const [densityScore] = useState(
+    Math.round((report.activeTime / report.recTime) * 100)
   );
 
   return (
@@ -46,7 +57,7 @@ export default function TrainingReportCard(props: TrainingReportCardProps) {
 
         {/* Report charts */}
         <Box width="100%" display="flex" alignItems="flex-start" gap={'1%'}>
-          <ChartContainer label="Realization">
+          <CustomChartContainer label="Realization">
             <PieChart
               height={120}
               hideLegend
@@ -70,15 +81,86 @@ export default function TrainingReportCard(props: TrainingReportCardProps) {
                 theme.palette.background.light,
               ]}
             >
+              <ChartsTooltip trigger="none" />
               <PieCenterLabel label={`${realizationScore}%`} />
             </PieChart>
-          </ChartContainer>
-          <ChartContainer label="Tonnage">
-            <></>
-          </ChartContainer>
-          <ChartContainer label="TODO">
-            <></>
-          </ChartContainer>
+          </CustomChartContainer>
+          <CustomChartContainer label="Tonnage">
+            <Box
+              display="flex"
+              flexDirection="column"
+              alignItems="center"
+              justifyContent="center"
+            >
+              <PieChart
+                height={75}
+                margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+                hideLegend
+                series={[
+                  {
+                    startAngle: -90,
+                    endAngle: 90,
+                    data: [
+                      { label: '', value: tonnageScore },
+                      { label: '', value: 100 - tonnageScore },
+                    ],
+                    innerRadius: 30,
+                    outerRadius: 45,
+                    cy: 55,
+                  },
+                ]}
+                colors={[
+                  theme.palette.primary.main,
+                  theme.palette.background.light,
+                ]}
+              >
+                <PieCenterLabel
+                  label={`${tonnageScore}kg`}
+                  position={{ top: 12 }}
+                />
+                <ChartsTooltip trigger="none" />
+              </PieChart>
+
+              <Typography
+                fontSize={12}
+                textAlign="center"
+                color={theme.palette.background.lightBorder}
+              >
+                Density
+              </Typography>
+              <ChartContainer
+                height={15}
+                series={[
+                  { type: 'bar', data: [densityScore], layout: 'horizontal' },
+                ]}
+                margin={0}
+                xAxis={[{ position: 'none', min: 0, max: 100 }]}
+                yAxis={[{ position: 'none', scaleType: 'band', data: [''] }]}
+                colors={[theme.palette.primary.main]}
+                sx={{
+                  backgroundColor: theme.palette.background.light,
+                }}
+              >
+                <BarPlot />
+                <ChartsTooltip trigger="item" />
+              </ChartContainer>
+            </Box>
+          </CustomChartContainer>
+          <Box
+            width="32.33%"
+            height={133}
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            gap={1}
+          >
+            <CustomValueBox
+              value={`${report.totalDuration}’`}
+              title="Duration"
+            />
+            <CustomValueBox value={`72%`} title="Intensity" />
+          </Box>
         </Box>
       </Box>
 
@@ -87,7 +169,7 @@ export default function TrainingReportCard(props: TrainingReportCardProps) {
   );
 }
 
-const ChartContainer = ({
+const CustomChartContainer = ({
   children,
   label,
 }: ChildrenProps & { label: string }) => {
@@ -101,12 +183,38 @@ const ChartContainer = ({
     >
       <Typography
         fontSize={12}
+        lineHeight={1}
         textAlign="center"
         color={theme.palette.background.lightBorder}
       >
         {label}
       </Typography>
       {children}
+    </Box>
+  );
+};
+
+const CustomValueBox = ({ value, title }: { value: string; title: string }) => {
+  return (
+    <Box
+      width={50}
+      height={50}
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      sx={{
+        backgroundColor: theme.palette.background.light,
+        border: `1px solid ${theme.palette.primary.main}`,
+        borderRadius: '5px',
+      }}
+    >
+      <Typography fontSize={14} textAlign="center">
+        {value}
+      </Typography>
+      <Typography fontSize={10} textAlign="center">
+        {title}
+      </Typography>
     </Box>
   );
 };
