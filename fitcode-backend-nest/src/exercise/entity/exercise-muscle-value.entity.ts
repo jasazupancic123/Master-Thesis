809d4b9ddf8f05cb7e6108578b40d190cc84c565
jeanbabtista <bedrac.zan@gmail.com>
2025-09-25 15:51:22 +0300

    feat: add institutionId to exercise

--- fitcode-backend-nest/src/exercise/entity/exercise-muscle-value.entity.ts
+++ fitcode-backend-nest/src/exercise/entity/exercise-muscle-value.entity.ts
@@ -5,6 +5,8 @@ import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 import { IdEntity } from '@src/common/entity/id.entity';
 
 export class ExerciseMuscleValue extends IdEntity {
+  // id is exerciseId
+
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
