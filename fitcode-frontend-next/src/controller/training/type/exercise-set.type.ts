6ad0d8b3dbf4322179347518e5ffd6d50ac9e6d7
jeanbabtista <bedrac.zan@gmail.com>
2025-10-01 12:40:28 +0300

    feat: change frontend types

--- fitcode-frontend-next/src/controller/training/type/exercise-set.type.ts
+++ fitcode-frontend-next/src/controller/training/type/exercise-set.type.ts
@@ -1,7 +1,30 @@
+import type { LoadType } from '../enum/load-type.enum';
 import type { AttributeValue } from '@/controller/attribute/type/attribute-value.type';
 
-export interface ExerciseSet {
-  setNumber: number;
-  paramValuesL: AttributeValue[];
-  paramValuesR?: AttributeValue[];
+export type ExerciseSet = ExerciseSetPrimarySide &
+  ExerciseSetSecondarySide & {
+    setNumber: number;
+    recTime: number; // in seconds
+    recDist?: number; // in meters, for distance-based recovery
+    loadType?: LoadType;
+
+    /* ---------------------- Deprecated ---------------------- */
+    paramValuesL: AttributeValue[];
+    paramValuesR?: AttributeValue[];
+  };
+
+export interface ExerciseSetPrimarySide {
+  reps: number;
+  load?: number; // e.g. weight in kg or percentage of 1RM or bodyweight
+  tempo?: string; // e.g. "2.5:0:3.5:0", meaning "eccentric:isometric:concentric:isometric" in seconds
+  time?: number; // for isometric holds, in seconds
+  dist?: number; // for distance-based sets, in meters
+}
+
+export interface ExerciseSetSecondarySide {
+  repsR?: number;
+  loadR?: number;
+  tempoR?: string;
+  timeR?: number;
+  distR?: number;
 }
--- fitcode-frontend-next/src/controller/training/type/training-exercise.type.ts
+++ fitcode-frontend-next/src/controller/training/type/training-exercise.type.ts
@@ -1,16 +1,15 @@
 import type { ExerciseSet } from './exercise-set.type';
-import type { ColorEntity, IdEntity } from '@/common/type/entity.type';
+import type { IdEntity } from '@/common/type/entity.type';
 import type { Attribute } from '@/controller/attribute/type/attribute.type';
 import type { Exercise } from '@/controller/exercise/type/exercise.type';
 
-export type TrainingExercise = IdEntity &
-  ColorEntity & {
-    params: Attribute[];
-    sets: ExerciseSet[];
-    attributes: Attribute[];
+export type TrainingExercise = IdEntity & {
+  params: Attribute[];
+  sets: ExerciseSet[];
+  attributes: Attribute[];
 
-    // mapped properties
-    exercise?: Exercise;
-  };
+  // mapped properties
+  exercise?: Exercise;
+};
 
 export type UpdateTrainingExercise = Pick<TrainingExercise, 'id' | 'sets'>;
