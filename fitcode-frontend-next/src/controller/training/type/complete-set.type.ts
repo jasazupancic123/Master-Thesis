3c7ae8a6cece01f5f40ca291d338e9cefd8e55db
jeanbabtista <bedrac.zan@gmail.com>
2025-10-02 09:44:07 +0300

    feat: tempo decimals as string

--- fitcode-frontend-next/src/controller/training/type/complete-set.type.ts
+++ fitcode-frontend-next/src/controller/training/type/complete-set.type.ts
@@ -8,11 +8,11 @@ export type CompleteLSet = {
   load?: number; // in kg
   rom?: number; // in cm
   velocity?: number; // in m/s
-  tempo?: number; // e.g. 2010 (for 2:0:1:0), average tempo
+  tempo?: string; // e.g. 2010 (for 2:0:1:0), average tempo
   photoUrl?: string;
 
   // the following fields are AI diagnostics
-  tempos?: number[]; // tempo for each rep
+  tempos?: string[]; // tempo for each rep
   roms?: number[]; // range of motion for each rep
   velocities?: number[]; // velocity for each rep
   feedback?: string[]; // feedback for each rep
@@ -25,11 +25,11 @@ export class CompleteRSet {
   loadR?: number; // in kg
   romR?: number; // in cm
   velocityR?: number; // in m/s
-  tempoR?: number; // e.g. 2010 (for 2:0:1:0)
+  tempoR?: string; // e.g. 2010 (for 2:0:1:0)
   photoUrlR?: string;
 
   // the following fields are AI diagnostics
-  temposR?: number[]; // tempo for each rep
+  temposR?: string[]; // tempo for each rep
   romsR?: number[]; // range of motion for each rep
   velocitiesR?: number[]; // velocity for each rep
   feedbackR?: string[]; // feedback for each rep
@@ -39,9 +39,6 @@ export type CompleteSet = Pick<WorkloadMeta, 'userId' | 'notes'> &
   Required<DateRange> &
   CompleteLSet &
   CompleteRSet & {
-    eff?: number; // in %
-    bpm?: number; // in bpm
-    mas?: number; // max aerobic speed (%)
-    recTime?: number; // in s
+    recTime: number; // in s
     recDist?: number; // in m
   };
