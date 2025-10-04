8cab2ead57d1b448fa88989477e3e01afb533aae
jeanbabtista <bedrac.zan@gmail.com>
2025-10-04 16:42:01 +0300

    chore

--- fitcode-backend-nest/src/firebase/get-firebase-client.ts
+++ fitcode-backend-nest/src/firebase/get-firebase-client.ts
@@ -35,6 +35,10 @@ export function getFirebaseClient(
     databaseId = configService.get('FIREBASE_DATABASE_ID');
   } else if (envCredentials)
     credential = admin.credential.cert(JSON.parse(envCredentials));
+  else
+    credential = admin.credential.cert(
+      require('../../serviceAccount-staging.json'),
+    );
 
   const apps = getApps();
   const app = (
