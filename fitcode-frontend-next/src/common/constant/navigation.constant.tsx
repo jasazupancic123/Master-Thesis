5396bd92571a7a5508eb6ee93bc86679b3c33509
jasazupancic123 <jasa.zupancic@student.um.si>
2025-10-06 17:19:06 +0300

    fix: small landing page design fixes

--- fitcode-frontend-next/src/common/constant/navigation.constant.tsx
+++ fitcode-frontend-next/src/common/constant/navigation.constant.tsx
@@ -42,22 +42,12 @@ export function link(
 // hero navbar
 export const LINK_PRODUCT = link('Product', '#product');
 export const LINK_SERVICES = link('Services', '#services');
-export const LINK_TECHNOLOGY = link(
-  'Technology',
-  '#technology',
-  null,
-  'technology'
-);
-export const LINK_CONTACT_US = link(
-  'Contact Us',
-  '#contact-us',
-  null,
-  'contact-us'
-);
-export const LINK_ABOUT_US = link('About Us', '#about-us', null, 'about-us');
+export const LINK_TECHNOLOGY = link('Technology', '/#technology', null, 'technology');
+export const LINK_CONTACT_US = link('Contact Us', '/#contact-us', null, 'contact-us');
+export const LINK_ABOUT_US = link('About Us', '/#about-us', null, 'about-us');
 
 // all standalone app links
-export const LINK_INDEX = link('Home', '#home', null, 'home');
+export const LINK_INDEX = link('Home', '/#home', null, 'home');
 export const LINK_SOLUTIONS = link('Solutions', '#solutions');
 export const LINK_PRODUCTS = link('Products', '#products');
 export const LINK_FEATURES = link('Features', '#features');
