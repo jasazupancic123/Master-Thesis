b06b108b1822599e2928f9558a9258e2ae2af2d4
Žan Bedrač <68808279+jeanbabtista@users.noreply.github.com>
2025-10-27 13:36:36 +0300

    Remove console logs from video error handling
    Removed console log statements for error handling.

--- fitcode-frontend-next/src/ui/file-upload.tsx
+++ fitcode-frontend-next/src/ui/file-upload.tsx
@@ -151,8 +151,6 @@ export default function FileUpload(props: Props) {
                 muted
                 style={{ objectFit: 'cover', width: '100%', height: '100%' }}
                 onError={(e) => {
-                  console.log('preview.url', preview.url);
-                  console.log('INVALID VIDEO ERROR', e);
                   setPreview((prev) => ({ ...prev, error: 'Invalid video' }));
                 }}
               />
