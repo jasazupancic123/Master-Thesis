03cb04b3c7637e004c25e2b61ba29a4d5d9252b0
jeanbabtista <bedrac.zan@gmail.com>
2025-10-30 16:37:03 +0300

    build: wrap magic link in suspense

--- fitcode-frontend-next/src/app/auth/magic/page.tsx
+++ fitcode-frontend-next/src/app/auth/magic/page.tsx
@@ -1,55 +1,11 @@
-'use client';
+import { Suspense } from 'react';
 
-import { useRouter, useSearchParams } from 'next/navigation';
-import { Suspense, useEffect, useState } from 'react';
-import toast from 'react-hot-toast';
-
-import { AuthController } from '@/core/auth/auth.controller';
-import { lib } from '@/lib';
-import { SIGN_IN_REDIRECT_MAPPER } from '@/lib/common/const/nav.const';
-import { useAuth } from '@/store/auth.provider';
+import MagicAuthPage from '@/sites/magic-auth.page';
 
 export default function Page() {
-  const router = useRouter();
-  const { handleUserChange } = useAuth();
-  const searchParams = useSearchParams();
-  const token = searchParams.get('token');
-
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (!token) {
-      setError('Missing token');
-      return;
-    }
-
-    async function login() {
-      try {
-        const { firebaseToken } = await AuthController.getInstance().verifyLink(
-          token!
-        );
-
-        const result =
-          await lib.firebase.auth.signInWithCustomToken(firebaseToken);
-        const idToken = await result.user.getIdToken();
-        const user = await AuthController.getInstance().sessionLogin(idToken);
-
-        const { role } = handleUserChange(user);
-        if (role) {
-          toast.success('Signed in successfully');
-          router.push(SIGN_IN_REDIRECT_MAPPER[role]?.href);
-        }
-      } catch (e) {
-        toast.error((e as Error).message);
-      }
-    }
-
-    login().then();
-  }, [token]);
-
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      {error ? <div>{error}</div> : <div>Logging you in...</div>}
+    <Suspense>
+      <MagicAuthPage />;
     </Suspense>
   );
 }
