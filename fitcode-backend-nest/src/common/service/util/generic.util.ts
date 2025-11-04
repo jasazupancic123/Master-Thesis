7ccdfa308a80b14770c465cca47755229fbc0eba
jeanbabtista <bedrac.zan@gmail.com>
2025-11-04 09:52:39 +0300

    chore: remove console log

--- fitcode-backend-nest/src/common/service/util/generic.util.ts
+++ fitcode-backend-nest/src/common/service/util/generic.util.ts
@@ -21,8 +21,6 @@ export class GenericUtil {
   }
 
   error(errors: ValidateError<any>[]): string {
-    console.log('errors:', errors);
-
     if (!errors.length) return '';
     return (
       this.upperFirstLetter(errors[0].message) +
