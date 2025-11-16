b3a87d4bb7f6c43514e1c470bc3e39be547e67ed
jeanbabtista <bedrac.zan@gmail.com>
2025-11-16 17:20:08 +0300

    feat: training protocols on backend

---
+++ fitcode-backend-nest/src/training/entity/training-protocol.entity.ts
@@ -0,0 +1,46 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Expose, Type } from 'class-transformer';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+
+import { IdEntity } from '@src/common/entity/id.entity';
+
+import { Superset } from './superset.entity';
+
+export class TrainingProtocol extends IdEntity {
+  @IsString()
+  @IsNotEmpty()
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Expose()
+  institutionId?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  @Expose()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  @Expose()
+  componentId: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Expose()
+  description?: string;
+
+  @ValidateNested({ each: true })
+  @Type(() => Superset)
+  @ApiProperty({ type: () => Superset, isArray: true })
+  @Expose()
+  supersets: Superset[]; // actual prescription of the training protocol
+}
