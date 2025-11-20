f3ebc340dc841ca308ba963d559f5f760e130fb2
jeanbabtista <bedrac.zan@gmail.com>
2025-11-20 06:54:54 +0100

    deploy: _ah/start endpoint wont start

--- fitcode-backend-nest/src/common/service/util/env.util.ts
+++ fitcode-backend-nest/src/common/service/util/env.util.ts
@@ -1,15 +1,22 @@
 import { config } from 'dotenv';
 
+import type { NodeEnv } from '@src/config/environment-validation-schema';
 import {
   type Environment,
-  type NodeEnv,
   validationSchema,
 } from '@src/config/environment-validation-schema';
 
 const nodeEnv = (process.env.NODE_ENV || 'dev') as NodeEnv;
-if (!['production', 'staging'].includes(nodeEnv))
+if (!['production', 'staging'].includes(nodeEnv)) {
   // in production and staging, the environment variables are set in other ways
+  console.log(`[env.util.ts] Loading .env file for environment: ${nodeEnv}`);
   config({ quiet: true, path: `.env.${nodeEnv}` });
+} else {
+  console.log(
+    `[env.util.ts] Running in ${nodeEnv} environment, skipping .env file loading`,
+  );
+  console.log('ENV:', process.env);
+}
 
 export class EnvUtil {
   // based on key return type, parse number/string/boolean
