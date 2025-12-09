229711d7aea0b5177bfde9354f48e018355dc82a
jeanbabtista <bedrac.zan@gmail.com>
2025-12-09 18:21:58 +0300

    fix: wellness bug

--- fitcode-frontend-next/src/store/main.provider.tsx
+++ fitcode-frontend-next/src/store/main.provider.tsx
@@ -216,7 +216,10 @@ export default function MainProvider(props: MainProviderProps) {
     institution,
     setInstitution,
     institutions: props.institutions,
-    wellness: users.data.map((p) => p.wellness).flat(),
+    wellness: users.data
+      .map((p) => p.wellness)
+      .filter(Boolean)
+      .flat(),
     activeTraining,
   };
 
