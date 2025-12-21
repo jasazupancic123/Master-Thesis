416ac8097214c74c1818b536bc8fe0a3d1b497f2
jasazupancic123 <jasa.zupancic@student.um.si>
2025-12-21 09:14:54 +0100

    chore: project init

--- face-encoder/fe_lib/subsystems/authentication/face_detection/__init__.py
+++
@@ -1,9 +0,0 @@
-from .base import DetectedFace, FaceDetector
-from .scrfd.scr_face_detector import SCRFaceDetector, SCRFaceDetectorConfig
-
-__all__ = [
-    "DetectedFace",
-    "FaceDetector",
-    "SCRFaceDetector",
-    "SCRFaceDetectorConfig",
-]
